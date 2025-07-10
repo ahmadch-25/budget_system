ad9191ded94fbd719fbbe3c177751b3d433c727f
Muhammad Ahmad <83905076+ahmadch-25@users.noreply.github.com>
2025-07-10 08:44:23 +0500

    Revert "budget app added"
    This reverts commit a8c4038a18e0c204d6280b403b773ce02ccac4be.

--- apps/budget/utils/helpers.py
+++
@@ -1,5 +0,0 @@
-def is_hour_in_range(start: int, end: int, hour: int) -> bool:
-    if start < end:
-        return start <= hour < end
-    else:  # wraparound, like 22–2
-        return hour >= start or hour < end
